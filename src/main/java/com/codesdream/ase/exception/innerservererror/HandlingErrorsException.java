b496b0317d4096e823b8129fdea9490eeb44d9ef
chuyan <1047381936@qq.com>
2020-09-01 15:35:37 +0800

    fourth deletion 数据库密码更换

--- src/main/java/com/codesdream/ase/exception/innerservererror/HandlingErrorsException.java
+++ src/main/java/com/codesdream/ase/exception/innerservererror/HandlingErrorsException.java
@@ -1,11 +1,13 @@
 package com.codesdream.ase.exception.innerservererror;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 // 处理错误对应的异常类
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class HandlingErrorsException extends RuntimeException {
     public HandlingErrorsException(String msg){
         super(msg);
